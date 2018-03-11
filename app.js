1824aff14be8d493344cba66dd2d108dfdd8e196
Alan Nguy <yaowong@hotmail.com>
2018-03-11 18:47:38 -0400

    updates

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('hi it works');
