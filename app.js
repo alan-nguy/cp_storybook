dbf712e754f079b0594e4156d3bcbbabaa8c1c2f
Alan Nguy <yaowong@hotmail.com>
2018-03-12 21:32:38 -0400

    added auth

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
 const port = process.env.PORT || 5000;
+const passport = require('passport');
+
+require('./config/passport')(passport);
+
+// load routes
+const auth = require('./routes/auth');
+
+app.use('/auth', auth);
 
 app.get('/', (req, res) => {
   res.send('hi it works');
 });
 
-app.listen(port, () => console.log(`Running on port ${port}`));
+
+
+app.listen(port, console.log(`Running on port ${port}`));
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://alan:nguy@ds263948.mlab.com:63948/storybook-dev',
+  googleClientID: '838885319216-ffoqqn1tft75f9mn4o4qqsqmqdopgnol.apps.googleusercontent.com',
+  googleClientSecret: 'bHSRQaquURrHSJUuYY0sIW_8'
+}
